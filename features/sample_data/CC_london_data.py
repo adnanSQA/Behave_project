026e33e0b1ed3f0df6227d497d2ac5db6931f58c
AdnanSQA <adnan.dev.py@gmail.com>
2024-08-04 20:12:22 +0500

    New Commit

--- features/sample_data/CC_london_data.py
+++ features/sample_data/CC_london_data.py
@@ -9,3 +9,4 @@ gaurantee_section_footer_text = "Call Our Local Carpet Cleaning Branch On 020 35
  
 additional_services_heading_text = "Additional Cleaning Services For Your Home"
 additional_services_estimates_text = "Call us for a Free Estimate"
+additional_services_footer_text = "Voted #1 carpet cleaner in London. Our customer service hours are 9.00am to 6.00pm 6 days a week."
